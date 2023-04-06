afeb5c0cbbf30bf57388970120b3050cf5c74b83
Alan Suarez <alansuarez641@hotmail.com>
2023-04-05 22:54:15 -0300

    Modify PHP

--- form.php
+++ form.php
@@ -1,22 +1,37 @@
 <?php
 
-// Recupera los datos del formulario
-$nombre = $_POST['nombre'];
-$telefono = $_POST['telefono'];
-$correo = $_POST['correo'];
-$tema = $_POST['tema'];
-$mensaje = $_POST['mensaje'];
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-// Construye el mensaje de correo electrónico
-$asunto = "Contacto Portfolio";
-$contenido = "Nombre: $nombre\n\nTeléfono: $telefono\n\nCorreo: $correo\n\nTema: $tema\n\nMensaje:\n$mensaje";
+require 'vendor/autoload.php'; // ruta a PHPMailer
 
-// Envía el correo electrónico
-$destinatario = "alansuarez641@hotmail.com";
-$headers = "From: $nombre <$correo>";
-mail($destinatario, $asunto, $contenido, $headers);
+// crea un nuevo objeto de correo electrónico
+$mail = new PHPMailer(true);
 
-// Redirige al usuario a una página de confirmación
-header("Location: pages/succes.html");
+try {
+  // Configura el servidor SMTP
+  $mail->isSMTP();
+  $mail->Host = 'smtp.gmail.com';
+  $mail->SMTPAuth = true;
+  $mail->Username = 'alansuarez641@hotmail.com';
+  $mail->Password = 'Newellsoldboys123';
+  $mail->SMTPSecure = 'tls';
+  $mail->Port = 587;
+
+  // Configura los datos del correo electrónico
+  $mail->setFrom('tu-email@gmail.com', 'Tu Nombre');
+  $mail->addAddress('alansuarez641@hotmail.com', 'Destinatario');
+  $mail->Subject = 'Nuevo mensaje del formulario';
+  $mail->Body = 'Nombre: ' . $_POST['nombre'] . "\r\n" .
+                'Email: ' . $_POST['email'] . "\r\n" .
+                'Mensaje: ' . $_POST['mensaje'];
+
+  // Envia el correo electrónico
+  $mail->send();
+  echo 'El mensaje se ha enviado correctamente';
+} catch (Exception $e) {
+  echo 'Error al enviar el mensaje: ' . $mail->ErrorInfo;
+}
+
 
 ?>
